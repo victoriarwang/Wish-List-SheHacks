45b7397a04d16e96eb8129a7aa94666acb3b8991
DaHee Kim <34751044+dahkimr@users.noreply.github.com>
2021-01-10 08:42:26 -0500

    Added @victoriarangers3's code

--- App.js
+++ App.js
@@ -1,18 +1,253 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Alert, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, SafeAreaView, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Card, Overlay } from 'react-native-elements';
 
 const styles = StyleSheet.create({
-  button: {
-    marginBottom: 20,
-    padding: 30
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
   },
-  space: {
-    width: 20, // or whatever size you need
-    height: 20,
-  }
+  gear: {
+    width: 35, height: 35,
+    marginLeft: 280, marginTop: 25,
+  },
+  logo: {
+    width: 180, height: 180, 
+    left: 10, top: 0,
+  },
+  welcomeEmma: {
+    fontFamily: 'Verdana',
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#157145',
+  },
+  hide: {
+    color: 'rgb(155, 208, 229)',
+  },
+  pfp: {
+    width: 100, height: 100, top: -20,
+  },
+  box: {
+    width: 414, height: 230,
+    backgroundColor: 'rgb(155, 208, 229)',
+    marginTop: -100,
+    position: 'absolute',
+    borderRadius: 30,
+  },
+  navbar: {
+    width: 414, height: 300,
+    backgroundColor: 'rgb(155, 208, 229)',
+    borderRadius: 30,
+    marginTop: 682,
+    position: 'absolute',
+  },
+  balance: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: -540,
+    marginLeft: -40,
+    position: 'absolute',
+  },
+  currentBalance: {
+    fontFamily: 'Verdana',
+    color: '#698eae',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: -120,
+    marginLeft: -100,
+    position: 'absolute',
+  },
+  buttonGPlusStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#dc4e41',
+    borderWidth: 1,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#157045',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 65,
+    borderRadius: 10,
+    margin: 5,
+    marginTop: -60,
+  },
+  buttonImageIconStyle: {
+    padding: 20,
+    margin: 10,
+    height: 25,
+    width: 20,
+    resizeMode: 'stretch',
+  },
+  buttonTextStyle: {
+    color: '#fff',
+    marginBottom: 4,
+    marginLeft: 40,
+    marginRight: 40,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  buttonIconSeparatorStyle: {
+    backgroundColor: '#fff',
+    width: 2,
+    height: 80,
+  },
+  circle: {
+    marginTop: -70,
+    marginLeft: 60,
+    position: 'absolute',
+    alignItems: 'center',
+    width: 300,
+    height: 200,
+    borderRadius: 80,
+    backgroundColor: 'rgb(155, 208, 229)',
+ },
+ circleleft: {
+    marginTop: 80,
+    marginLeft: 100,
+    position: 'absolute',
+    alignItems: 'center',
+    width: 100,
+    height: 200,
+    borderRadius: 80,
+    backgroundColor: 'rgb(155, 208, 229)',
+ },
+ circleright: {
+  marginTop: 80,
+  marginLeft: 400,
+  position: 'absolute',
+  alignItems: 'center',
+  width: 100,
+  height: 200,
+  borderRadius: 80,
+  backgroundColor: 'rgb(155, 208, 229)',
+},
+ loginScreenButton:{
+  marginRight:40,
+  marginLeft:40,
+  marginTop:17,
+  paddingTop:50,
+  paddingBottom:60,
+  paddingLeft:210,
+  paddingRight:50,
+  backgroundColor:'#157045',
+  borderRadius:80,
+},
+loginTextSave:{
+    color:'#fff',
+    textAlign:'center',
+    top: -10,
+    left: -60,
+    fontSize: 25,
+    fontWeight: 'bold',
+
+},
+loginTextSpend:{
+  color:'#fff',
+  textAlign:'center',
+  top: 103,
+  left: -125,
+  fontSize: 25,
+  fontWeight: 'bold',
+},
+loginTextShare:{
+  color:'#fff',
+  textAlign:'center',
+  top: 73,
+  left: 76,
+  fontSize: 25,
+  fontWeight: 'bold',
+},
+triangle: {
+  width: 0,
+  height: 0,
+  marginLeft: 0,
+  marginTop: 122,
+  backgroundColor: 'transparent',
+  borderStyle: 'solid',
+  borderRightWidth: 110,
+  borderBottomWidth: 80,
+  borderLeftWidth: 110,
+  borderTopColor: 'transparent',
+  borderRightColor: 'transparent',
+  borderBottomColor: '#157045',
+  borderLeftColor: 'transparent',
+  transform: [{ rotate: '180deg'}],
+  position: 'absolute',
+},
+left: {
+  marginTop: 50,
+  marginLeft: -140,
+  width: 105, height: 50,
+  borderRadius: 40,
+},
+right: {
+  marginTop: -46,
+  marginLeft: 215,
+  width: 100, height: 50,
+  borderRadius: 40,
+},
+save: {
+  marginTop: -210,
+  marginLeft: 25,
+  width: 70, height: 70,
+  borderRadius: 40,
+  position: 'absolute',
+},
+savetext: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  color: 'white',
+  marginLeft: -80,
+  marginTop: -75,
+},
+spend: {
+  marginTop: -60,
+  marginLeft: -160,
+  width: 45, height: 45,
+  borderRadius: 40,
+},
+spendtext: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  color: 'white',
+  marginLeft: 180,
+  marginTop: 181,
+  position: 'absolute',
+},
+share: {
+  marginTop: 100,
+  marginLeft: 110,
+  width: 40, height: 40,
+  borderRadius: 40,
+  position: 'absolute',
+},
+sharetext: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  color: 'white',
+  marginLeft: -200,
+  marginTop: 57,
+  position: 'absolute',
+},
+donatetext: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  color: 'white',
+  marginTop: 7,
+  marginLeft: 315,
+}
 });
 
 const taskStyles = StyleSheet.create({
@@ -194,18 +429,111 @@ const wishStyles = StyleSheet.create({
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10 }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="WishList"
-        onPress={() => navigation.navigate('WishList')}
-      />
-      <View style={styles.space} />
-      <Button
-        title="Tasks"
-        onPress={() => navigation.navigate('Tasks')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.box}>
+        <Text style={styles.hide}>
+          hi!
+        </Text>
+      </SafeAreaView>
+      <View>
+        <Image
+        style={styles.gear}
+          source={require('./assets/gear.png')}
+        />
+      </View>
+      <View>
+        <Image
+          style={styles.pfp}
+          source={require('./assets/girl.png')}
+        />
+      </View>
+      <View>
+        <Text style={styles.welcomeEmma}>
+          WELCOME EMMA!
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require('./assets/piggybank.png')}
+        />
+      </View>
+      <View><Text style={styles.currentBalance}>Current Balance</Text></View>
+
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigation.navigate('WishList')}
+          style={styles.button}
+          activeOpacity={0.5}>
+
+          <Text style={styles.buttonTextStyle}>
+            Wish List
+          </Text>
+          <View style={styles.buttonIconSeparatorStyle} />
+
+          <Image
+            source={require('./assets/wishlist.png')}
+            style={styles.buttonImageIconStyle}
+          />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text style={styles.balance}>$70.00</Text>
+      </View>
+      <SafeAreaView style={styles.navbar}>
+      <View style={styles.circle}>
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          onPress={() => console.log('pressed')}
+          underlayColor='#fff'>
+            
+        </TouchableOpacity>
+        
+      
+        <Text style={styles.savetext}>
+          SAVE
+        </Text>
+        
+      </View>
+
+      <View style={styles.left}>
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          onPress={() => console.log('pressed')}
+          underlayColor='#fff'>
+        </TouchableOpacity>
+        
+      </View>
+
+      <View style={styles.right}>
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          onPress={() => console.log('pressed')}
+          underlayColor='#fff'>
+        </TouchableOpacity>
+      
+        <Text style={styles.sharetext}>
+          SHARE
+        </Text>
+      </View>
+      
+         
+      <Text style={styles.donatetext}>
+          SPEND
+        </Text>
+      </SafeAreaView>
+      
+      <View>
+        <Image
+        style={styles.save}
+          source={require('./assets/save.png')}
+        />
+      </View>
+
+      
+      
+      <StatusBar style="auto" />
+
+    </SafeAreaView>
   );
 }
 
