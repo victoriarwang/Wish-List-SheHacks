929eb88d31a0753a2d268cd0be69a546a780da45
syang486 <syang486@uwo.ca>
2021-01-09 21:27:27 -0500

    Tasks page update

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -36,8 +36,59 @@ function WishListScreen() {
 
 function TasksScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Tasks Screen</Text>
+
+    
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
+
+      <Text style={{fontSize: 25, fontWeight: 'bold'}}>{"\n"}Available Tasks</Text>
+
+      <ScrollView horizontal={true} style={{flex: 0.1}}> 
+      <TouchableOpacity onPress={() => Alert.alert('Sweep the floor: $5')}>
+      <Image style={styles.tinyLogo} source={require('./Images/sweep.png')} /> 
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => Alert.alert('Wash the dishes: $8')}>
+      <Image style={styles.tinyLogo} source={require('./Images/dishes.png')} /> 
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => Alert.alert('Put the laundry away: $10')}>
+      <Image style={styles.tinyLogo} source={require('./Images/laundry.png')} /> 
+      </TouchableOpacity>
+      </ScrollView> 
+      
+      <TouchableOpacity
+      style={styles.buttonRequestTask}
+      activeOpacity={0.5}>
+        <Image source={require('./Images/thumb.png')}
+        style={styles.buttonRequestImageIconStyle}
+        />
+        <View style={styles.buttonRequestIconSeparatorStyle} />
+        <Text style={styles.buttonRequestTextStyle}>COMPLETED TASKS</Text>
+      </TouchableOpacity>
+
+      <View style={{flex: 0.1}}/>
+      <TouchableOpacity
+      style={styles.buttonRequestTask}
+      activeOpacity={0.5}>
+        <Image source={require('./Images/notify.png')}
+        style={styles.buttonRequestImageIconStyle}
+        />
+        <View style={styles.buttonRequestIconSeparatorStyle} />
+        <Text style={styles.buttonRequestTextStyle}>REQUEST NEW TASKS</Text>
+      </TouchableOpacity>
+
+      <View style={{flex: 0.1}}/>
+      <View style={{flexDirection: 'row', textAlign: 'left'}}>
+        <Image source={require('./Images/mark.png')} style={{width: 40, height: 40}} />
+        <Text style={{fontSize: 25, fontWeight: 'bold'}}>Notification from Mom:</Text>
+      </View>
+      <View style={{paddingRight: 105, flexDirection: 'row', textAlign: 'left'}}>
+        <Image source={require('./Images/dot.png')} style={{width: 23, height: 23}} />
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}> Sweep the floor</Text>
+      </View>
+      <View style={{flex: 0.01}} />
+      <View style={{ flexDirection: 'row', textAlign: 'left'}}>
+        <Image source={require('./Images/dot.png')} style={{width: 23, height: 23}} />
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}> Wash the dishes before 5 pm</Text>
+      </View>
     </View>
   );
 }
@@ -75,5 +126,39 @@ const styles = StyleSheet.create({
   space: {
     width: 20, // or whatever size you need
     height: 20,
+  },
+  tinyLogo: {
+    width: 200,
+    height: 200,
+    margin: 5
+  },
+  buttonRequestTask:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#485a96',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: 40,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonRequestImageIconStyle:{
+    padding: 10,
+    margin: 5,
+    height: 30,
+    width: 30,
+    resizeMode: 'stretch',
+  },
+  buttonRequestTextStyle: {
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 4,
+    marginLeft: 10,
+    marginRight: 10, 
+  },
+  buttonRequestIconSeparatorStyle:{
+    backgroundColor: '#fff',
+    width: 1,
+    height: 40,
   },
 })
